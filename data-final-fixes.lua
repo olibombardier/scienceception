49a572e9b7706a2686dde9a38f08141d490d3a69
Olivier Bombardier <olibombardier@gmail.com>
2025-01-26 17:37:04 -0500

    ignores cerys science pack

--- data-final-fixes.lua
+++
@@ -1,5 +0,0 @@
-local update_data = require("main")
-
-if mods["Cerys-Moon-of-Fulgora"] then
-	update_data()
-end
--- data-updates.lua
+++ data-updates.lua
@@ -1,5 +1,3 @@
 local update_data = require("main")
 
-if not mods["Cerys-Moon-of-Fulgora"] then
-	update_data()
-end
+update_data()
